d24c7c11723348e7e9019e2a9255ebda2375c91c
Murray Smith <murray@mismith.info>
2014-12-15 12:08:03 -0700

    improved firebaseHelper caching

--- assets/js/modules/firebaseHelper.js
+++ assets/js/modules/firebaseHelper.js
@@ -1,6 +1,6 @@
 angular.module('firebaseHelper', ['firebase'])
 
-	.service('$firebaseHelper', ['$firebase', function($firebase){
+	.service('$firebaseHelper', ['$firebase', '$q', function($firebase, $q){
 		var self   = this,
 			namespace = '',
 			cached = {};
@@ -13,9 +13,8 @@ angular.module('firebaseHelper', ['firebase'])
 		// returns: Reference
 		self.$ref = function(){
 			var args = Array.prototype.slice.call(arguments);
-			//if( args[0] != 'constants') args.unshift('data');
 			
-			var path = 'Ref' + args.join('');
+			var path = 'Ref/' + args.join('/');
 			if(cached[path]) return cached[path];
 			
 			var $ref = new Firebase('https://' + namespace + '.firebaseio.com/' + (args.join('/') || ''));
@@ -26,14 +25,28 @@ angular.module('firebaseHelper', ['firebase'])
 		
 		// returns: Instance
 		self.$inst = function(){
-			var args = Array.prototype.slice.call(arguments),
-				path = 'Inst' + args.join('');
-			if(cached[path]) return cached[path];
+			if(arguments.length == 1 && arguments[0] instanceof Firebase){
+				// accept/handle firebase $ref as argument too, not just string(s)
+				var ref  = arguments[0],
+					path = 'Inst' + ref.path;
+				
+				if(cached[path]) return cached[path];
+				
+				var $inst = $firebase(ref);
+				cached[path] = $inst;
 			
-			var $inst = $firebase(self.$ref.apply(this, arguments));
-			cached[path] = $inst;
+				return $inst;
+			}else{
+				// handle string(s)
+				var args = Array.prototype.slice.call(arguments),
+					path = 'Inst/' + args.join('/');
+				if(cached[path]) return cached[path];
+				
+				var $inst = $firebase(self.$ref.apply(this, args));
+				cached[path] = $inst;
 			
-			return $inst;
+				return $inst;
+			}
 		};
 		
 		// returns: Object or Array
@@ -48,7 +61,7 @@ angular.module('firebaseHelper', ['firebase'])
 			}
 			
 			// retrieve cached item, if possible
-			var path = type + args.join('');
+			var path = type + '/' + args.join('/');
 			if(cached[path]) return cached[path];
 			
 			// retrieve from remote, then cache it for later
@@ -65,8 +78,6 @@ angular.module('firebaseHelper', ['firebase'])
 		
 		// returns: Instance
 		self.$child = function(){
-			// @TODO: cache somehow
-			
 			var args = Array.prototype.slice.call(arguments),
 				parent = args.shift();
 			
@@ -77,16 +88,51 @@ angular.module('firebaseHelper', ['firebase'])
 				parent = parent.$ref();
 			}
 			if(angular.isFunction(parent.child)){ // it's a Firebase Reference
-				return $firebase(parent.child(args.join('/')));
+				return self.$inst(parent.child(args.join('/')));
 			}
 			return parent; // fallback to parent
 		};
 		
+		self.$populate = function(keys, values, cbAdded){
+			var array   = [],
+				keysRef = self.$ref(keys);
+			
+			// fire callback even if no keys found
+			keysRef.once('value', function(snapshot){
+				if( ! angular.isObject(snapshot.val())){
+					if(angular.isFunction(cbAdded)) cbAdded();
+				}
+			});
+			
+			// watch for additions/deletions at keysRef
+			keysRef.on('child_added', function(snapshot){
+				var $item = self.$get(values, snapshot.name());
+				
+				$item.$loaded().then(function(){
+					var deferreds = [];
+					if(angular.isFunction(cbAdded)) deferreds.push(cbAdded($item));
+					
+					$q.all(deferreds).then(function(){
+						array.push($item);
+					});
+				});
+			});
+			keysRef.on('child_removed', function(snapshot){
+				array.splice($rootScope.childById(array, snapshot.name(), undefined, true), 1);
+			});
+			return array;
+		};
 		
-		self.$intersect = function(keysPath, valuesPath){
+		self.$intersect = function(keysPath, valuesPath, keysMap, valuesMap){
 			// @TODO: cache somehow
 			
-			return $firebase(Firebase.util.intersection(self.$ref(keysPath), self.$ref(valuesPath))).$asArray();
+			var keysObj   = {ref: self.$ref(keysPath)},
+				valuesObj = {ref: self.$ref(valuesPath)};
+			
+			if(keysMap)   keysObj.keyMap   = keysMap;
+			if(valuesMap) valuesObj.keyMap = valuesMap;
+			
+			return $firebase(Firebase.util.intersection(keysObj, valuesObj)).$asArray();
 		};
 		
 		return self;
